d3d88dd7c1fe2dd9a5884bcf5d338d24cbae6b3a
dahowie <andreas.svarholt@gmail.com>
2020-10-25 12:46:51 +0100

    init

---
+++ old.tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    colors: {
+      primary: 'var(--color-primary)',
+      secondary: 'var(--color-secondary)',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
